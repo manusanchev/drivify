7b281b385c96c779eb51e086081a1191674e2fb4
Manuel Sánchez Velázquez <49983557+manusanchev@users.noreply.github.com>
2020-06-10 21:30:39 +0200

    Landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,92 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('auth'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('auth') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <link href="{{ asset('css/auth/landing.css') }}" rel="stylesheet">
+</head>
+<body>
+    <header class="container-fluid">
+        <div class="d-flex justify-content-between align-items-center py-2 px-5">
+            <div class="container">
+                <h3 class="text-white">Drivify</h3>
+            </div>
+            <div class="d-flex justify-content-around container align-items-center w-25 mr-0">
+                <a href="/login" class="text-white text-decoration-none btn">Login</a>
+                <a href="/register" class="text-white text-decoration-none btn">Registro</a>
             </div>
         </div>
-    </body>
+        <div class="d-flex justify-content-center align-items-center h-75">
+            <h1 class="text-white title">Viaja con gente con gustos musicales diferentes</h1>
+        </div>
+    </header>
+   
+  <section class="features" id="features">
+    <div class="container">
+      <div class="section-heading text-center">
+        <h2>Recursos ilimitados, diversion ilimitada</h2>
+        <p class="text-muted">Prueba nuestra web, no tendras que pelear mas por la musica</p>
+        <hr>
+      </div>
+      <div class="row">
+        <div class="col my-auto">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="feature-item">
+                  <i class="icon-screen-smartphone text-primary"></i>
+                  <h3>¡Vamonos de viaje!</h3>
+                  <p class="text-muted">Añade tu salida y tu destino, comparte el codigo con tus ocupantes y elige tus canciones</p>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="feature-item">
+                  <i class="icon-camera text-primary"></i>
+                  <h3>Uso intuitivo</h3>
+                  <p class="text-muted">Revise sus viajes, edite su perfil, y mucho mas</p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="feature-item">
+                  <i class="icon-present text-primary"></i>
+                  <h3>Totalmente gratis</h3>
+                  <p class="text-muted">No te preocupes por el dinero. Es totalmente gratis hoy y para siempre</p>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="feature-item">
+                  <i class="icon-lock-open text-primary"></i>
+                  <h3>Tus datos para ti</h3>
+                  <p class="text-muted">En Drivify no tendras que preocuparte por tus datos personales.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="cta">
+    <div class="cta-content">
+      <div class="container">
+        <h2>No esperes mas.<br>Empieza a usar Drivify.</h2>
+        <a href="/register" class="btn btn-outline btn-xl js-scroll-trigger">A viajar se ha dicho</a>
+      </div>
+    </div>
+    <div class="overlay"></div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <h3 class="text-white">&copy; Drivify</h3>
+    </div>
+  </footer>
+
+</body>
 </html>
